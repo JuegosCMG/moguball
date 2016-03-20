202e05984cfd1e2073fd03fde25ec212b25e8ab3
garfenter <garfenter@gmail.com>
2016-03-20 11:46:30 -0600

    ADD: SE AGREGA VERSION INICIAL DE MOGUBALL

---
+++ desktop/src/com/moguca/moguball/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.moguca.moguball.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.moguca.moguball.MoguBallGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MoguBallGame(), config);
+	}
+}
